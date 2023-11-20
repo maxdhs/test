4e2f193649723d209fde80ac53386ff7fada0bd5
Maxwell Dubin <maxdhs@gmail.com>
2023-11-20 07:40:33 -0500

    initialize

---
+++ index.js
@@ -0,0 +1,6 @@
+import { add } from "./add.js";
+import minus from "./minus.js";
+
+// can't create any additional files without the terminal. all new functions have to be in their own modules
+console.log(add(2, 2));
+console.log(minus(4, 2));
---
+++ minus.js
@@ -0,0 +1,5 @@
+const minus = (num1, num2) => {
+  return num1 - num2;
+};
+
+export default minus;
