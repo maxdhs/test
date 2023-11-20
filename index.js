f059b891cbb1ad2f74b852ce176c5b825f37d2be
Maxwell Dubin <maxdhs@gmail.com>
2023-11-20 07:48:22 -0500

    add divide function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { add } from "./add.js";
+import { divide } from "./divide.js";
 import minus from "./minus.js";
 
 // can't create any additional files without the terminal. all new functions have to be in their own modules
 console.log(add(2, 2));
 console.log(minus(4, 2));
+console.log(divide(4, 2));
